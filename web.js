f512c7952af9c67835272941144b998c5f4c8795
Ubuntu <ubuntu@domU-12-31-39-16-C1-25.compute-1.internal>
2013-07-05 19:11:25 +0000

    modify to read from file

--- web.js
+++ web.js
@@ -1,12 +1,19 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
+var fs = require('fs');
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  //response.send('Hello World 2!');
+  response.send(str);
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+data = fs.readFileSync('index.html');
+str = data.toString()
+//console.log("Read...."+ data);
+//console.log("Read...."+ str);
